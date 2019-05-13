72c41994aa4166e4af9012005dfdea02841ec573
diehlms <diehlstx@gmail.com>
2019-05-12 22:15:33 -0500

    corrected routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
+  get '/contacts', to: 'static_pages#contacts'
+  get '/event', to: 'static_pages#event'
+  get '/gallery', to: 'static_pages#gallery'
+  get '/history', to: 'static_pages#history'
+  get '/join', to: 'static_pages#join'
+  get '/sale', to: 'static_pages#sale'
+  get '/specs', to: 'static_pages#specs'
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/contacts'
-  get 'static_pages/event'
-  get 'static_pages/gallery'
-  get 'static_pages/history'
-  get 'static_pages/join'
-  get 'static_pages/sale'
-  get 'static_pages/specs'
 end
