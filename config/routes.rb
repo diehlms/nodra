4d5a7091728ea3718930791ecb1d99f5f2f6f5e4
diehlms <diehlstx@gmail.com>
2019-05-19 11:17:12 -0500

    changed routing again

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy', as: 'logout'
   get '/contacts', to: 'static_pages#contacts'
   get '/event', to: 'static_pages#event'
   get '/gallery', to: 'static_pages#gallery'
