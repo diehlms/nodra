f3a2f3215a9ca138dbfdb7c000812399f3446477
diehlms <diehlstx@gmail.com>
2019-05-21 18:00:33 -0500

    changed comment styling

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :article
 
-    validates :comment, presence: true, allow_blank: false
+    validates :comment, presence: true, allow_blank: false, length: { maximum: 1000}
 end
