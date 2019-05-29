c548c077fe38a609515b4778c01611524c9f110c
diehlms <diehlstx@gmail.com>
2019-05-28 21:49:00 -0500

    changed styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:success] = "You have logged in"
-      redirect_to users_path(user)
+      redirect_to '/blog'
     else
       flash[:danger] = "There was something wrong with your login information"
       render 'new'
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,7 @@
 class Picture < ApplicationRecord
     mount_uploader :image, ImageUploader
     serialize :image, JSON
+
+    validates :title, presence: true, length: { maximum: 30 }
+    validates :description, presence: true, length: { maximum: 120 }
 end
