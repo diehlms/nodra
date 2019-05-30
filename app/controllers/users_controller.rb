3af35def674fa6cda65c78deab9bd430ede5aa97
diehlms <diehlstx@gmail.com>
2019-05-29 20:02:34 -0500

    configured sendgrid

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
       if @user.save
-        UserMailer.registration_confirmation(@user).deliver_now
+        UserMailer.registration_confirmation(@user).deliver
         flash.now[:success] = "Please confirm your email address to continue"
         redirect_to root_url
       else
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -41,6 +41,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "localhost:3000" }
 
   config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.perform_deliveries = true
   
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
