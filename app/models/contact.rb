195d53eccacfb9fe69022b854a7b1d2debf5955f
diehlms <diehlstx@gmail.com>
2019-06-19 19:29:30 -0500

    updated phone regex

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,10 @@
 class Contact < ApplicationRecord
 
+    PHONE_REGEX = /((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}/
+
     validates :name, presence: true
     validates :email, presence: true
     validates :fleet, presence: true
     validates :phone_number, presence: true, format: { with: PHONE_REGEX }
 
-    PHONE_REGEX = /((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}/
 end
