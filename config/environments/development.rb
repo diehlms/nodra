8bf1a2c56c5fa4f8e95c029047f0adfabe3db8ec
diehlms <diehlstx@gmail.com>
2019-06-24 18:01:53 -0500

    updated styling

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,5 +65,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  require 'carrierwave/orm/activerecord'
 end
